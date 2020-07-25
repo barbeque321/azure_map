b0c37937f9a9ad477d670f156f217da2d591725a
barbeque321 <adambbq@gmail.com>
2020-07-25 21:13:16 +0200

    req

--- azure_map/azure_map_project/urls.py
+++ azure_map/azure_map_project/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from azure_map_project import views
 
+
+
+app_name = 'azure_map'
 urlpatterns = [
     path('', views.azure_map_project, name='azure_map_project'),
+    # path('', views.current_datetime, name='current_datetime'),
 ]
--- azure_map/azure_map_project/views.py
+++ azure_map/azure_map_project/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+from django.template import loader
 
 # Create your views here.
 def azure_map_project(request):
     return render(request, 'azure_map_project.html', {})
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    current_date = "<html><body>It is now %s.</body></html>" % now
+    template = loader.get_template('azure_map_project.html')
+    context = {
+        'current_date': current_date,
+    }
+    return HttpResponse(template.render(context, request))
