ab382dc0eceb6a93eb2ea811a2901c6b985d2584
barbeque321 <adambbq@gmail.com>
2020-07-25 16:50:25 +0200

    req

--- azure_map/settings.py
+++ azure_map/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_URL = 'wwwroot/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'wwwroot/static'), ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # Application definition
 
 INSTALLED_APPS = [
--- azure_map/urls.py
+++ azure_map/urls.py
@@ -13,11 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('azure_map_project.urls')),
+urlpatterns = [i18n_patterns(path('admin/', admin.site.urls), prefix_default_language=False) + \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    path('', include('azure_map_project.urls'))
+    ]
 
-]
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('azure_map_project.urls')),
+
+# ]
