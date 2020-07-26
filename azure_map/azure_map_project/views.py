e583cd7d10fc48593e69ada75989a26d224a3dd8
barbeque321 <adambbq@gmail.com>
2020-07-26 11:27:08 +0200

    some

--- azure_map/azure_map_project/views.py
+++ azure_map/azure_map_project/views.py
@@ -7,16 +7,6 @@ from django.template import loader
 def azure_map_project(request):
     return render(request, 'azure_map_project.html', {})
 
-def current_datetime(request):
-    now = datetime.datetime.now()
-    current_date = "<html><body>It is now %s.</body></html>" % now
-    template = loader.get_template('azure_map_project.html')
-    context = {
-        'current_date': current_date,
-    }
-    return HttpResponse(template.render(context, request))
-
-
 def generate(request):
 template = loader.get_template('azure_map_project.html')	
 location = request.GET.get('theAdress')
