0b6b30d708d692f626915154492d41b0b16b9de1
barbeque321 <adambbq@gmail.com>
2020-07-26 10:42:34 +0200

    some

--- azure_map/azure_map_project/urls.py
+++ azure_map/azure_map_project/urls.py
@@ -3,10 +3,12 @@ from azure_map_project import views
 from django.urls import include, path, re_path
 from django.conf.urls import url
 
-app_name = 'azure_map_project'
+
 
 urlpatterns = [
     url('', views.azure_map_project, name='azure_map_project'),
     url('', views.generate, name='generate'),
     # path('', views.current_datetime, name='current_datetime'),
 ]
+
+app_name = 'azure_map_project'
