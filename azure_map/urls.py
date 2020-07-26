2f83b3ce7060e3510d7ae4629a8b699f3aa36785
barbeque321 <adambbq@gmail.com>
2020-07-26 10:55:34 +0200

    some

--- azure_map/urls.py
+++ azure_map/urls.py
@@ -9,6 +9,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-    url('azure_map_project/', include('azure_map_project.urls', namespace="azure_map_project") ),
-    
+    # url('azure_map_project/', include('azure_map_project.urls', namespace="azure_map_project") ),
+    path('azure_map_project/', include(("azure_map_project.urls", 'azure_map_project'), namespace="azure_map_project")),
 ]
