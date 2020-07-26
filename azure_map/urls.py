c7068be61a33ef6d307062618254353a8d21881a
barbeque321 <adambbq@gmail.com>
2020-07-26 10:27:04 +0200

    some

--- azure_map/urls.py
+++ azure_map/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
 from django.conf import settings
+from django.conf.urls import patterns, url
 
 
+app_name = 'azure_map'
 
 urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
